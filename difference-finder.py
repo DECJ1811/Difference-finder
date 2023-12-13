850d04f557e1fc17924277fa051b3c028d189503
DECJ1811 <152626996+DECJ1811@users.noreply.github.com>
2023-12-13 17:14:47 -0500

    Add files via upload

---
+++ difference-finder.py
@@ -0,0 +1,28 @@
+from PIL import Image
+#original image and image with difference
+input_image=Image.open("C:\\Users\\15cd005la\\Downloads\\original.PNG")
+difference=Image.open("C:\\Users\\15cd005la\\Downloads\\different.PNG")
+
+
+
+widthA, heightB= input_image.size
+widthC, heightD= difference.size
+min_width= min(widthA, widthC)
+min_height= min(heightB, heightD)
+input_image= input_image.resize((min_width, min_height))
+difference= difference.resize((min_width, min_height))
+width1, height1= input_image.size
+width2, height2= difference.size
+
+Original_map= input_image.load()
+difference_map= difference.load()
+for i in range(width2):
+    for j in range(height2):
+        r1, g1, b1, p1 = input_image.getpixel((i, j))
+        r2, g2, b2, p1 = difference.getpixel((i, j))
+        sum_of_colors1= sum([r1, g1, b1])
+        sum_of_colors2 = sum([r2, g2, b2])
+        grayscale = (0.299 * r2 + 0.587 * g2 + 0.114 * b2)
+        if sum_of_colors2 not in range(sum_of_colors1-110, sum_of_colors1+110):
+            difference_map[i, j]=(int(grayscale), int(grayscale), int(grayscale))
+difference.save("C:\\Users\\15cd005la\\Downloads\\diferences.PNG", format="png")
